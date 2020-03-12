8d6221e247e14262a489aa6c7662e36a7fb4f5ba
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-12 14:55:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
-def reverse_each_word ("Hello there, and how are you?")
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+def 
+reverse_each_word (sentence)
+  sentence.split.collect do 
+|backward_words|
+  backward_words.reverse 
+  end
+    .join(" ")
+
 end 
 
   
